654ad419333831090e72036c9d5e54c96edef49e
John Wang <i@foundjohn.com>
2012-05-07 01:22:23 +0000

    minishell

--- shell.c
+++ shell.c
@@ -1,213 +1,358 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <signal.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <ctype.h>
-#include <errno.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
-extern int errno;
-
-typedef void (*sighandler_t)(int);
-static char *my_argv[100], *my_envp[100];
-static char *search_path[10];
-
-void handle_signal(int signo)
+#include "defines.h"
+#include "functions.h"
+#define MAXLINE 4096
+void handleUserCommand()
 {
-	printf("\n[MY_SHELL ] ");
-	fflush(stdout);
+ //       if( chk_pipe()==1)
+   //     {
+
+        if (checkBuiltInCommands() == 0) {
+		//printf("%d",commandArgc);
+		// puts("chkbuiltin works");
+
+                launchJob(commandArgv, "STANDARD", 0, FOREGROUND);
+                              //puts(commandArgv[1]);
+        }
 }
 
-void fill_argv(char *tmp_argv)
-{
-	char *foo = tmp_argv;
-	int index = 0;
-	char ret[100];
-	bzero(ret, 100);
-	while(*foo != '\0') {
-		if(index == 10)
-			break;
+ //int check_pipe()
+ //{
+ void pipelining(int flag)
+ {
+    char    line[MAXLINE];
+    FILE    *fpin, *fpout;
+    char arg1[50],arg2[50];
+    int i;
+   if(flag==2)
+   {
+	    strcat(arg1,commandArgv[0]);
+	    strcpy(arg2,commandArgv[2]);
+   }
+    else
+   {
+	   for(i=0;i<flag-1;i++)
+	   {
+		   strcat(arg1,commandArgv[i]);
+		   strcat(arg1," ");
+	   }
+	   strcpy(arg2,commandArgv[commandArgc-1]);
+   }
+  // puts(commandArgv[0]);
+   //puts(arg1);
+   //puts(arg2);
 
-		if(*foo == ' ') {
-			if(my_argv[index] == NULL)
-				my_argv[index] = (char *)malloc(sizeof(char) * strlen(ret) + 1);
-			else {
-				bzero(my_argv[index], strlen(my_argv[index]));
-			}
-			strncpy(my_argv[index], ret, strlen(ret));
-			strncat(my_argv[index], "\0", 1);
-			bzero(ret, 100);
-			index++;
-		} else {
-			strncat(ret, foo, 1);
-		}
-		foo++;
-		/*printf("foo is %c\n", *foo);*/
-	}
-	my_argv[index] = (char *)malloc(sizeof(char) * strlen(ret) + 1);
-	strncpy(my_argv[index], ret, strlen(ret));
-	strncat(my_argv[index], "\0", 1);
+    if ((fpin = popen(arg1, "r")) == NULL)
+        printf("can't open %s", arg1);
+
+    if ((fpout = popen(arg2, "w")) == NULL)
+        printf("popen error");
+
+
+    while (fgets(line, MAXLINE, fpin) != NULL) {
+        if (fputs(line, fpout) == EOF)
+            printf("fputs error to pipe");
+    }
+    if (ferror(fpin))
+        printf("fgets error");
+    if (pclose(fpout) == -1)
+       printf("");
+
+
+    //return 1;
 }
 
-void copy_envp(char **envp)
+
+int checkBuiltInCommands()
 {
-	int index = 0;
-	for(;envp[index] != NULL; index++) {
-		my_envp[index] = (char *)malloc(sizeof(char) * (strlen(envp[index]) + 1));
-		memcpy(my_envp[index], envp[index], strlen(envp[index]));
-	}
-}
+        if (strcmp("exit", commandArgv[0]) == 0) {
+                exit(EXIT_SUCCESS);
+        }
+        if (strcmp("cd", commandArgv[0]) == 0) {
 
-void get_path_string(char **tmp_envp, char *bin_path)
+                changeDirectory();
+                return 1;
+        }
+       /* if (strcmp("in", commandArgv[0]) == 0) {
+                launchJob(commandArgv + 2, *(commandArgv + 1), STDIN, FOREGROUND);
+                return 1;
+        }
+        if (strcmp("out", commandArgv[0]) == 0) {
+                launchJob(commandArgv + 2, *(commandArgv + 1), STDOUT, FOREGROUND);
+                return 1;
+        }*/
+        if (strcmp("bg", commandArgv[0]) == 0) {
+                if (commandArgv[1] == NULL)
+                        return 0;
+                if (strcmp("in", commandArgv[1]) == 0)
+                        launchJob(commandArgv + 3, *(commandArgv + 2), STDIN, BACKGROUND);
+                else if (strcmp("out", commandArgv[1]) == 0)
+                        launchJob(commandArgv + 3, *(commandArgv + 2), STDOUT, BACKGROUND);
+                else
+                        launchJob(commandArgv + 1, "STANDARD", 0, BACKGROUND);
+                return 1;
+        }
+        if (strcmp("fg", commandArgv[0]) == 0) {
+                if (commandArgv[1] == NULL)
+                        return 0;
+                int jobId = (int) atoi(commandArgv[1]);
+                t_job* job = getJob(jobId, BY_JOB_ID);
+                if (job == NULL)
+                        return 0;
+                if (job->status == SUSPENDED || job->status == WAITING_INPUT)
+                        putJobForeground(job, TRUE);
+                else                                                                                                // status = BACKGROUND
+                        putJobForeground(job, FALSE);
+                return 1;
+        }
+        if (strcmp("jobs", commandArgv[0]) == 0) {
+                printJobs();
+                return 1;
+        }
+        if (strcmp("kill", commandArgv[0]) == 0)
+        {
+                if (commandArgv[1] == NULL)
+                        return 0;
+                killJob(atoi(commandArgv[1]));
+                return 1;
+        }
+        //int i;
+        ////for( i=0;i<=commandArgc;i++)
+        //{
+        //char *str;
+        //strcpy(str,commandArgv);
+        if( commandArgc == 3 )
+        {
+		if(strcmp("|", commandArgv[1]) == 0  )
+            {
+            pipelining(2);
+            return 1;
+            }
+            //return 1;
+        }
+	if( commandArgc > 3 )
 {
-	int count = 0;
-	char *tmp;
-	while(1) {
-		tmp = strstr(tmp_envp[count], "PATH");
-		if(tmp == NULL) {
-			count++;
-		} else {
-			break;
-		}
-	}
-        strncpy(bin_path, tmp, strlen(tmp));
-}
-
-void insert_path_str_to_search(char *path_str) 
-{
-	int index=0;
-	char *tmp = path_str;
-	char ret[100];
-
-	while(*tmp != '=')
-		tmp++;
-	tmp++;
-
-	while(*tmp != '\0') {
-		if(*tmp == ':') {
-			strncat(ret, "/", 1);
-			search_path[index] = (char *) malloc(sizeof(char) * (strlen(ret) + 1));
-			strncat(search_path[index], ret, strlen(ret));
-			strncat(search_path[index], "\0", 1);
-			index++;
-			bzero(ret, 100);
-		} else {
-			strncat(ret, tmp, 1);
-		}
-		tmp++;
-	}
-}
-
-int attach_path(char *cmd)
-{
-	char ret[100];
-	int index;
-	int fd;
-	bzero(ret, 100);
-	for(index=0;search_path[index]!=NULL;index++) {
-		strcpy(ret, search_path[index]);
-		strncat(ret, cmd, strlen(cmd));
-		if((fd = open(ret, O_RDONLY)) > 0) {
-			strncpy(cmd, ret, strlen(ret));
-			close(fd);
-			return 0;
-		}
-	}
-	return 0;
-}
-
-void call_execve(char *cmd)
-{
-	int i;
-	printf("cmd is %s\n", cmd);
-	if(fork() == 0) {
-		i = execve(cmd, my_argv, my_envp);
-		printf("errno is %d\n", errno);
-		if(i < 0) {
-			printf("%s: %s\n", cmd, "command not found");
-			exit(1);		
-		}
-	} else {
-		wait(NULL);
-	}
-}
-
-void free_argv()
-{
-	int index;
-	for(index=0;my_argv[index]!=NULL;index++) {
-		bzero(my_argv[index], strlen(my_argv[index])+1);
-		my_argv[index] = NULL;
-		free(my_argv[index]);
-	}
-}
-
-int main(int argc, char *argv[], char *envp[])
-{
-	char c;
-	int i, fd;
-	char *tmp = (char *)malloc(sizeof(char) * 100);
-	char *path_str = (char *)malloc(sizeof(char) * 256);
-	char *cmd = (char *)malloc(sizeof(char) * 100);
-	
-	signal(SIGINT, SIG_IGN);
-	signal(SIGINT, handle_signal);
-
-	copy_envp(envp);
-	get_path_string(my_envp, path_str);
-	insert_path_str_to_search(path_str);
-
-	if(fork() == 0) {
-		execve("/usr/bin/clear", argv, my_envp);
-		exit(1);
-	} else {
-		wait(NULL);
-	}
-	printf("[MY_SHELL ] ");
-	fflush(stdout);
-	while(c != EOF) {
-		c = getchar();
-		switch(c) {
-			case '\n': if(tmp[0] == '\0') {
-					   printf("[MY_SHELL ] ");
-				   } else {
-					   fill_argv(tmp);
-					   strncpy(cmd, my_argv[0], strlen(my_argv[0]));
-					   strncat(cmd, "\0", 1);
-					   if(index(cmd, '/') == NULL) {
-						   if(attach_path(cmd) == 0) {
-							   call_execve(cmd);
-						   } else {
-							   printf("%s: command not found\n", cmd);
-						   }
-					   } else {
-						   if((fd = open(cmd, O_RDONLY)) > 0) {
-							   close(fd);
-							   call_execve(cmd);
-						   } else {
-							   printf("%s: command not found\n", cmd);
-						   }
-					   }
-					   free_argv();
-					   printf("[MY_SHELL ] ");
-					   bzero(cmd, 100);
-				   }
-				   bzero(tmp, 100);
-				   break;
-			default: strncat(tmp, &c, 1);
-				 break;
+		if(strcmp("|", commandArgv[commandArgc-2]) == 0 )
+		{
+			//puts("works");
+			//printf("%d",commandArgc);
+			pipelining(commandArgc-1);
+			return 1;
 		}
 	}
-	free(tmp);
-	free(path_str);
-	for(i=0;my_envp[i]!=NULL;i++)
-		free(my_envp[i]);
-	for(i=0;i<10;i++)
-		free(search_path[i]);
-	printf("\n");
-	return 0;
+        //break;
+        //}
+
+
+
+        return 0;
+}
+
+void executeCommand(char *command[], char *file, int newDescriptor,
+                    int executionMode)
+{
+        int commandDescriptor;
+        if (newDescriptor == STDIN) {
+                commandDescriptor = open(file, O_RDONLY, 0600);
+                dup2(commandDescriptor, STDIN_FILENO);
+                close(commandDescriptor);
+        }
+        if (newDescriptor == STDOUT) {
+                commandDescriptor = open(file, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+                dup2(commandDescriptor, STDOUT_FILENO);
+                close(commandDescriptor);
+        }
+        if (execvp(*command, command) == -1)
+                perror("MSH");
+}
+
+void launchJob(char *command[], char *file, int newDescriptor,
+               int executionMode)
+{
+        pid_t pid;
+        pid = fork();
+        switch (pid) {
+        case -1:
+                perror("MSH");
+                exit(EXIT_FAILURE);
+                break;
+        case 0:
+                signal(SIGINT, SIG_DFL);
+                signal(SIGQUIT, SIG_DFL);
+                signal(SIGTSTP, SIG_DFL);
+                signal(SIGCHLD, &signalHandler_child);
+                signal(SIGTTIN, SIG_DFL);
+                usleep(20000);
+                setpgrp();
+                if (executionMode == FOREGROUND)
+                        tcsetpgrp(MSH_TERMINAL, getpid());
+                if (executionMode == BACKGROUND)
+                        printf("[%d] %d\n", ++numActiveJobs, (int) getpid());
+
+                executeCommand(command, file, newDescriptor, executionMode);
+
+                exit(EXIT_SUCCESS);
+                break;
+        default:
+                setpgid(pid, pid);
+
+                jobsList = insertJob(pid, pid, *(command), file, (int) executionMode);
+
+                t_job* job = getJob(pid, BY_PROCESS_ID);
+
+                if (executionMode == FOREGROUND) {
+                        putJobForeground(job, FALSE);
+                }
+                if (executionMode == BACKGROUND)
+                        putJobBackground(job, FALSE);
+                break;
+        }
+}
+
+void putJobForeground(t_job* job, int continueJob)
+{
+        job->status = FOREGROUND;
+        tcsetpgrp(MSH_TERMINAL, job->pgid);
+        if (continueJob) {
+                if (kill(-job->pgid, SIGCONT) < 0)
+                        perror("kill (SIGCONT)");
+        }
+
+        waitJob(job);
+        tcsetpgrp(MSH_TERMINAL, MSH_PGID);
+}
+
+void putJobBackground(t_job* job, int continueJob)
+{
+        if (job == NULL)
+                return;
+
+        if (continueJob && job->status != WAITING_INPUT)
+                job->status = WAITING_INPUT;
+        if (continueJob)
+                if (kill(-job->pgid, SIGCONT) < 0)
+                        perror("kill (SIGCONT)");
+
+        tcsetpgrp(MSH_TERMINAL, MSH_PGID);
+}
+
+void waitJob(t_job* job)
+{
+        int terminationStatus;
+        while (waitpid(job->pid, &terminationStatus, WNOHANG) == 0) {
+                if (job->status == SUSPENDED)
+                        return;
+        }
+        jobsList = delJob(job);
+}
+
+void killJob(int jobId)
+{
+        t_job *job = getJob(jobId, BY_JOB_ID);
+        kill(job->pid, SIGKILL);
+}
+
+void changeDirectory()
+{
+        if (commandArgv[1] == NULL) {
+                chdir(getenv("HOME"));
+        } else {
+                if (chdir(commandArgv[1]) == -1) {
+                        printf(" %s: no such directory\n", commandArgv[1]);
+                }
+        }
+}
+
+
+void init()
+{
+        MSH_PID = getpid();
+        MSH_TERMINAL = STDIN_FILENO;
+        MSH_IS_INTERACTIVE = isatty(MSH_TERMINAL);
+
+        if (MSH_IS_INTERACTIVE) {
+                while (tcgetpgrp(MSH_TERMINAL) != (MSH_PGID = getpgrp()))
+                        kill(MSH_PID, SIGTTIN);
+
+                signal(SIGQUIT, SIG_IGN);
+                signal(SIGTTOU, SIG_IGN);
+                signal(SIGTTIN, SIG_IGN);
+                signal(SIGTSTP, SIG_IGN);
+                signal(SIGINT, SIG_IGN);
+                signal(SIGCHLD, &signalHandler_child);
+
+                setpgid(MSH_PID, MSH_PID);
+                MSH_PGID = getpgrp();
+                if (MSH_PID != MSH_PGID) {
+                        printf("Error, the shell is not process group leader");
+                        exit(EXIT_FAILURE);
+                }
+                if (tcsetpgrp(MSH_TERMINAL, MSH_PGID) == -1)
+                        tcgetattr(MSH_TERMINAL, &MSH_TMODES);
+
+                currentDirectory = (char*) calloc(1024, sizeof(char));
+        } else {
+                printf("Could not make MSH interactive. Exiting..\n");
+                exit(EXIT_FAILURE);
+        }
+}
+
+int main(int argc, char **argv, char **envp)
+{
+        init();
+        welcomeScreen();
+        shellPrompt();
+        while (TRUE) {
+                userInput = getchar();
+                switch (userInput) {
+                case '\n':
+                        shellPrompt();
+                        break;
+                default:
+                        getTextLine();
+                        handleUserCommand();
+                        shellPrompt();
+                        break;
+                }
+        }
+        printf("\n");
+        return 0;
+}
+
+void signalHandler_child(int p)
+{
+        pid_t pid;
+        int terminationStatus;
+        pid = waitpid(WAIT_ANY, &terminationStatus, WUNTRACED | WNOHANG);
+        if (pid > 0) {
+                t_job* job = getJob(pid, BY_PROCESS_ID);
+                if (job == NULL)
+                        return;
+                if (WIFEXITED(terminationStatus)) {
+                        if (job->status == BACKGROUND) {
+                                printf("\n[%d]+  Done\t   %s\n", job->id, job->name);
+                                jobsList = delJob(job);
+                        }
+                } else if (WIFSIGNALED(terminationStatus)) {
+                        printf("\n[%d]+  KILLED\t   %s\n", job->id, job->name);
+                        jobsList = delJob(job);
+                } else if (WIFSTOPPED(terminationStatus)) {
+                        if (job->status == BACKGROUND) {
+                                tcsetpgrp(MSH_TERMINAL, MSH_PGID);
+                                changeJobStatus(pid, WAITING_INPUT);
+                                printf("\n[%d]+   suspended [wants input]\t   %s\n",
+                                       numActiveJobs, job->name);
+                        } else {
+                                tcsetpgrp(MSH_TERMINAL, job->pgid);
+                                changeJobStatus(pid, SUSPENDED);
+                                printf("\n[%d]+   stopped\t   %s\n", numActiveJobs, job->name);
+                        }
+                        return;
+                } else {
+                        if (job->status == BACKGROUND) {
+                                jobsList = delJob(job);
+                        }
+                }
+                tcsetpgrp(MSH_TERMINAL, MSH_PGID);
+        }
 }
